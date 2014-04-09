78c2c82aec5108c832cfb5ad2f40f8261ab4cdf3
Ray Meghani <rehman.a.meghani@gmail.com>
2014-04-09 10:35:02 -0500

    added customer and orders classes

---
+++ EntityCRUD.DataModels/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityCRUD.DataModels
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+
+        //reverse lookup
+        public virtual List<Order> Orders { get; set; }
+    }
+}
---
+++ EntityCRUD.DataModels/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityCRUD.DataModels
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string Description { get; set;}
+        public DateTime OrderDate { get; set;}
+        public double OrderTotal { get; set;}
+
+        //foreign key
+        public int CustomerId { get; set; }
+    }
+}
