cf241830743d4039f36c99c4560ad18527de2e80
J. Andrew Brassington <jabbrass@zoho.com>
2014-04-09 10:02:11 -0500

    Init Commit

---
+++ EntityCRUD.Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using EntityCRUD.DataModels;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityCRUD.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection")
+        {
+        }
+
+
+    }
+}
