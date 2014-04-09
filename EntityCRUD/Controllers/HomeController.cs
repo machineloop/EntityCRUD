79b5bf9d3391abc7d4da884b2d430eb17139e3b6
J. Andrew Brassington <jabbrass@zoho.com>
2014-04-09 15:59:44 -0500

    Created CRUD Methods on Home Controller

--- EntityCRUD/Controllers/HomeController.cs
+++ EntityCRUD/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using EntityCRUD.Data;
+using EntityCRUD.DataModels;
+using EntityCRUD.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +11,82 @@ namespace EntityCRUD.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index(string searchString)
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            HomeIndexViewModel viewmodel = new HomeIndexViewModel();
+
+            if (searchString != null)
+            {
+                viewmodel.Customers = db.Customers.Include("Orders")
+                                                  .Where(c => c.Name.Contains(searchString))
+                                                  .ToList();
+            }
+            else
+            {
+            viewmodel.Customers = db.Customers.ToList();
+            viewmodel.Orders = db.Orders.ToList();
+            }
+
+            return View(viewmodel);
+        }
+
+        
+        public ActionResult NewCustomer()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult NewCustomer(Customer newcustomer)
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            HomeIndexViewModel viewmodel = new HomeIndexViewModel();
+
+            db.Customers.Add(newcustomer);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult UpdateCustomer(int id)
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            HomeIndexViewModel viewmodel = new HomeIndexViewModel();
+
+            viewmodel.Customers = db.Customers.Where(c => c.Id == id).ToList();
+
+            return View(viewmodel);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateCustomer(int id, Customer updatedcustomer)
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            HomeIndexViewModel viewmodel = new HomeIndexViewModel();
+            Customer customer = new Customer();
+
+            customer = db.Customers.Find(id);
+            customer.Name = updatedcustomer.Name;
+            customer.Birthday = updatedcustomer.Birthday;
+
+            db.SaveChanges();
+
+            return View(viewmodel);
+        }
+
+        public ActionResult DeleteCustomer(int id)
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            HomeIndexViewModel viewmodel = new HomeIndexViewModel();
+
+            Customer deletecustomer = db.Customers.Find(id);
+
+            db.Customers.Remove(deletecustomer);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
         }
 
         public ActionResult About()
---
+++ EntityCRUD/Models/HomeIndexViewModel.cs
@@ -0,0 +1,22 @@
+﻿using EntityCRUD.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EntityCRUD.Models
+{
+    public class HomeIndexViewModel
+    {
+        public List<Customer> Customers { get; set; }
+        public List<Order> Orders { get; set; }
+        //public List<Customer> Customers1 { get; set; }
+        //public List<Order> Orders1 { get; set; }
+        //public List<Customer> Customers2 { get; set; }
+        //public List<Order> Orders2 { get; set; }
+        //public List<Customer> Customers3 { get; set; }
+        //public List<Order> Orders3 { get; set; }
+        //public List<Customer> Customers4 { get; set; }
+        //public List<Order> Orders4 { get; set; }
+    }
+}
