d60b2d3a6988261e17322d90b819424627111727
J. Andrew Brassington <jabbrass@zoho.com>
2014-04-10 14:59:49 -0500

    Added order crud

---
+++ EntityCRUD/Controllers/OrderController.cs
@@ -0,0 +1,77 @@
+﻿using EntityCRUD.Data;
+using EntityCRUD.DataModels;
+using EntityCRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EntityCRUD.Controllers
+{
+    public class OrderController : Controller
+    {       
+        ApplicationDbContext db = new ApplicationDbContext();
+        OrderViewModel model = new OrderViewModel();
+
+        // GET: Order
+        public ActionResult Index()
+        {
+            model.VMOrders = db.Orders.ToList();
+            return View(model);
+        }
+
+        // GET: Order
+        public ActionResult AddOrder()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddOrder(string description, double ordertotal)
+        {
+            model.VMOrder = new Order();
+
+            model.VMOrder.OrderDate = DateTime.Now;
+            model.VMOrder.CustomerId = 1;
+
+            model.VMOrder.Description = description;
+            model.VMOrder.OrderTotal = ordertotal;
+
+            db.Orders.Add(model.VMOrder);
+            db.SaveChanges();
+
+            ViewBag.SuccessMessage = "Congrats, you just added an order!";
+            return View(model);
+        }
+        [HttpGet]
+        public ActionResult EditOrder(int id)
+        {
+            model.VMOrder = new Order();
+            model.VMOrder = db.Orders.Find(id);
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult EditOrder(int id, string description, double ordertotal)
+        {
+            model.VMOrder = db.Orders.Find(id);
+
+            model.VMOrder.Description = description;
+            model.VMOrder.OrderTotal = ordertotal;
+
+            db.SaveChanges();
+
+            ViewBag.SuccessMessage = "Congrats, you just edited an order!";
+            return View(model);
+        }
+
+        public ActionResult DeleteOrder(int id)
+        {
+            Order order = db.Orders.Find(id);
+            db.Orders.Remove(order);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ EntityCRUD/Models/OrderViewModel.cs
@@ -0,0 +1,14 @@
+﻿using EntityCRUD.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EntityCRUD.Models
+{
+    public class OrderViewModel
+    {
+        public List<Order> VMOrders { get; set; }
+        public Order VMOrder { get; set; }
+    }
+}
